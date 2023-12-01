008a99c228ea7466ee4a1bb3c4004ef8b32e146c
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2023-12-01 09:14:15 +0530

    Add files via upload

---
+++ DEMO1.java
@@ -0,0 +1,16 @@
+package asfaak;
+import java.util.Scanner;
+
+public class DEMO1 {
+
+	public static void main(String[] args) {
+	Scanner input = new Scanner (System.in);
+	System.out.print("Input your first name: ");
+	String fname = input.next();
+	System.out.print("Input your last name: ");
+	String lname = input.next();
+	System.out.println();
+	System.out.println("HI  \n"+fname+""+lname);
+	}
+
+}
