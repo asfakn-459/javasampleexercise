284b4a4e84d10d2ed4dfafc76da210e306e2c767
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2023-12-16 17:55:10 +0530

    Add files via upload

---
+++ DEMO4.java
@@ -0,0 +1,13 @@
+package asfaak;
+
+public class DEMO4 {
+	public static void main(String[] args) {
+		int w = -5 + 8 * 6;
+		int x = (55+9) % 9;
+		int y = 20 + (-3%5 / 8);
+		int z = 5 - 5 + 5 * 5 / 5;
+		
+		System.out.println(w + "\n" + x + "\n" + y + "\n" +z);
+	}
+
+}
