7b1273c54eaaaa7816653be22ad6a25b93e92718
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2023-12-29 19:40:09 +0530

    Add files via upload

---
+++ DEMO16.java
@@ -0,0 +1,21 @@
+package asfaak;
+
+public class DEMO16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String[] arra = new String[5];
+		
+		arra[0] = " +\"\"\"\"\"+ ";
+		arra[1] = "[| o o |]";
+		arra[2] = " |  ^  | ";
+		arra[3] = " | '-' | ";
+		arra[4] = " +-----+ ";
+		
+	for (int i = 0; i < 5; i++) {
+		System.out.println(arra[i]);
+		}
+	}
+
+}
