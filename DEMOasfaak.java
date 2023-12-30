00d67ce2391f344756c8687c621048c04f159ae8
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2023-12-30 19:24:29 +0530

    Add files via upload

---
+++ DEMOasfaak.java
@@ -0,0 +1,15 @@
+package asfaak;
+
+public class DEMOasfaak {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for( int n=0; n<10; n++) {
+		for( int i=0; i<10; i++) {
+				System.out.println((i+1) + " x " + (n+1) + " = " + (i*n));
+				
+			}
+		}
+	}
+
+}
