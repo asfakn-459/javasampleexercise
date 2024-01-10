6a33d5a19b896c7e7f60d8e54a30e495f0fcc735
asfakn-459 <147117318+asfakn-459@users.noreply.github.com>
2024-01-10 08:53:43 +0530

    Add files via upload

---
+++ StringConctenation.java
@@ -0,0 +1,9 @@
+public class StringConctenation {
+    public static void main(String[] args) {
+        String s1 = 10+20 +"Welcome" +40+10 ;
+                String s2 = "to java";
+                String s3 = "Welcome";
+                System.out.println(s1+s2);
+                System.out.println(s3.concat ( s2));
+    }
+}
