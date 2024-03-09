0a88a709eead27bc0c045edb3c4637c31b245125
Asfak <147117318+asfakn-459@users.noreply.github.com>
2024-03-09 21:52:08 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int x=0;
+        int j=10;
+        int i[]={10,20,30,04};
+        System.out.println("the length of array is " + i.length);
+        for(x=0; x<=i.length-1;x++ )
+            System.out.println(i[x]);
+
+    }
+}
